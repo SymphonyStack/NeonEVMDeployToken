aebb1b06bbd145ef2140896294547062eecd7c9d
Vedant Mahadik <mahadikvedant32@gmail.com>
2024-11-16 14:51:11 +0530

    ft: init

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,39 +1,47 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 const args = process.argv.slice(2);
-console.log(args)
-let privateKey = "0x0000000000000000000000000000000000000000000000000000000000000000";
-if (process.argv.slice(2)[0] === 'deploy') {
-    privateKey = args[6]
-    console.log(privateKey)
-    if (!privateKey) {
-        throw new Error("Private key not provided. Use --private-key argument or set PRIVATE_KEY in .env file.");
-    }
+console.log(args);
+let privateKey =
+  "0x0000000000000000000000000000000000000000000000000000000000000000";
+if (process.argv.slice(2)[0] === "deploy") {
+  privateKey = args[6];
+  console.log(privateKey);
+  if (!privateKey) {
+    throw new Error(
+      "Private key not provided. Use --private-key argument or set PRIVATE_KEY in .env file.",
+    );
+  }
 }
 
 module.exports = {
-    solidity: "0.8.20",
-    networks: {
-        mumbai: {
-            url: 'https://rpc-amoy.polygon.technology/',
-            accounts: [`${privateKey}`]
-        }
-    }
+  solidity: "0.8.20",
+  networks: {
+    neondevnet: {
+      url: "https://devnet.neonevm.org",
+      accounts: [`${privateKey}`],
+      chainId: 245022926,
+    },
+  },
 };
 
 task("deploy", "Deploys the MyToken contract")
-    .addPositionalParam("name", "The name of the token")
-    .addPositionalParam("symbol", "The symbol of the token")
-    .addPositionalParam("initialSupply", "The initial supply of the token")
-    .addPositionalParam("privateKey", "The private key of the deployer")
-    .setAction(async(taskArgs) => {
-        console.log(taskArgs)
-        const [deployer] = await ethers.getSigners();
+  .addPositionalParam("name", "The name of the token")
+  .addPositionalParam("symbol", "The symbol of the token")
+  .addPositionalParam("initialSupply", "The initial supply of the token")
+  .addPositionalParam("privateKey", "The private key of the deployer")
+  .setAction(async taskArgs => {
+    console.log(taskArgs);
+    const [deployer] = await ethers.getSigners();
 
-        console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Deploying contracts with the account:", deployer.address);
 
-        const Token = await ethers.getContractFactory("MyToken");
-        const token = await Token.deploy(taskArgs.name, taskArgs.symbol, taskArgs.initialSupply);
+    const Token = await ethers.getContractFactory("MyToken");
+    const token = await Token.deploy(
+      taskArgs.name,
+      taskArgs.symbol,
+      taskArgs.initialSupply,
+    );
 
-        console.log("Token deployed to:", token.address);
-    });
+    console.log("Token deployed to:", token.address);
+  });
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,33 @@
 async function main() {
-    const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners();
 
-    console.log("Deploying contracts with the account:", deployer.address);
-    console.log('2')
-    const args = process.argv.slice(2);
-    const name = args[4];
-    const symbol = args[5];
-    const initialSupply = args[6];
-    console.log('2')
-    console.log(name)
-    console.log(symbol)
-    console.log(initialSupply)
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("2");
+  const args = process.argv.slice(2);
+  const name = args[4];
+  const symbol = args[5];
+  const initialSupply = args[6];
+  console.log("2");
+  console.log(name);
+  console.log(symbol);
+  console.log(initialSupply);
 
-    if (!name || !symbol || !initialSupply) {
-        throw new Error("Please provide the name, symbol, and initial supply as arguments");
-    }
+  if (!name || !symbol || !initialSupply) {
+    throw new Error(
+      "Please provide the name, symbol, and initial supply as arguments",
+    );
+  }
 
-    const Token = await ethers.getContractFactory("MyToken");
-    const token = await Token.deploy(name, symbol, initialSupply);
+  const Token = await ethers.getContractFactory("MyToken");
+  const token = await Token.deploy(name, symbol, initialSupply);
 
-    console.log("Token deployed to:", token.address);
+  console.log("Token deployed to:", token.address);
+  console.log("##", { address: token.address }, "##");
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
